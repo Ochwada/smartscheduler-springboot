8ce60657c4411649fa1992592075346bea15c899
ochwada <nakhulolinda@yahoo.com>
2025-06-24 15:04:46 +0200

    added Person class and updated Tasks

---
+++ src/main/java/com/example/smartscheduler/model/Person.java
@@ -0,0 +1,17 @@
+package com.example.smartscheduler.model;
+
+
+/**
+ * *******************************************************
+ *  Package: com.example.smartscheduler.model
+ *  File: Person.java
+ *  Author: Ochwada
+ *  Date: Tuesday, 24.Jun.2025, 2:55 PM
+ *  Description: 
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public class Person {
+}
