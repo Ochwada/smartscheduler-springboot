210fb39dc5191a7a01a3a0ced8694212724e949c
ochwada <nakhulolinda@yahoo.com>
2025-06-24 15:08:50 +0200

    added Person class and updated Tasks

---
+++ src/main/java/com/example/smartscheduler/repo/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.example.smartscheduler.repo;
+
+
+/**
+ * *******************************************************
+ *  Package: com.example.smartscheduler.repo
+ *  File: PersonRepository.java
+ *  Author: Ochwada
+ *  Date: Tuesday, 24.Jun.2025, 3:07 PM
+ *  Description: 
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public class PersonRepository {
+}
